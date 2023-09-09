1fbf3a5bc66c85da89fa6e63ac7a8cf583cf0a66
andrey4281 <Andrey4281@yandex.ru>
2023-09-09 17:10:53 +0200

    Init

---
+++ kube.tf
@@ -0,0 +1,32 @@
+module "kube" {
+  source     = "git@github.com:terraform-yc-modules/terraform-yc-kubernetes.git"
+  network_id = module.yc-vpc.vpc_id
+
+  master_locations  = [
+    {
+      zone      = lookup(module.yc-vpc.private_subnets["10.11.0.0/24"], "zone")
+      subnet_id = lookup(module.yc-vpc.private_subnets["10.11.0.0/24"], "subnet_id")
+    }
+  ]
+
+  master_maintenance_windows = [
+    {
+      day        = "monday"
+      start_time = "23:00"
+      duration   = "3h"
+    }
+  ]
+
+  node_groups = {
+    "ambulance-worker-nodes"  = {
+      description   = "Ambulance worker nodes group"
+      auto_scale    = {
+        min         = 3
+        max         = 7
+        initial     = 3
+      }
+      max_expansion   = 1
+      max_unavailable = 1
+    }
+  }
+}
