2dae18c0220dec6a764e8dd7af4565fde9e8e18e
andrey4281 <Andrey4281@yandex.ru>
2023-09-10 11:35:02 +0200

    Add gitlab pipiline. Fix

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+module "yc-vpc" {
+  source              = "./modules/yc-vpc"
+  network_name        = "ambulance-network"
+  network_description = "ambulance-network created with module"
+  private_subnets     = [
+    {
+      name           = "subnet-1"
+      zone           = "ru-central1-a"
+      v4_cidr_blocks = ["10.10.0.0/24"]
+    },
+    {
+      name           = "subnet-2"
+      zone           = "ru-central1-b"
+      v4_cidr_blocks = ["10.11.0.0/24"]
+    },
+    {
+      name           = "subnet-3"
+      zone           = "ru-central1-c"
+      v4_cidr_blocks = ["10.12.0.0/24"]
+    }
+  ]
+}
+
 module "kube" {
   source     = "./modules/kube"
   network_id = module.yc-vpc.vpc_id
